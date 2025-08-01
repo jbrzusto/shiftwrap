jbrzusto/shiftwrap

commit 51ba356cb08aa56f48362a9d178cf56e9b6c65fd

don't skip ShiftChanges when timing is tight
For a pair of closely-spaced ShiftChanges where the first was calculated
from one day, and the second from another, the second was often skipped
because after generating a new day of shiftchanges, the next relevant
shiftchange was sought using the .trueAt time of the last triggered
shiftchange, which could be considerably later than its nominal .At time
This was especially so for dilated clocks, but would also happen with
back-to-back shifts.

Now, use the ordering of the .At fields to find the next ShiftChange so
that we don't skip any.

shiftwrap.go
@@ -1383,7 +1383,7 @@ func (sw *ShiftWrap) doShiftChange(now time.Time) {
 		// log.Printf("stopping shift %s at %v", sc.Shift().Label, sw.Clock.Now())
 		sw.Stop(s, sc.shift, now)
 	}
-	if !sw.advanceShiftChange(s, now) {
+	if !sw.advanceShiftChange(s, now, sc) {
 		log.Printf("stopping management of service %s", s.Name)
 		sw.doUnmanageService(s)
 	}
@@ -1393,7 +1393,7 @@ func (sw *ShiftWrap) doShiftChange(now time.Time) {
 // new shift changes if at the end of the shift change list.  It will look for up to
 // a year for shifts, printing an error and returning false if no shift is found.
 // Otherwise, returns true.
-func (sw *ShiftWrap) advanceShiftChange(s *Service, now time.Time) bool {
+func (sw *ShiftWrap) advanceShiftChange(s *Service, now time.Time, sc ShiftChange) bool {
 	s.shiftChangeIndex++
 	// log.Printf("shift change index for %s: %d, len=%d\n", s.Name, s.shiftChangeIndex, len(s.shiftChanges))
 	if s.shiftChangeIndex >= len(s.shiftChanges) {
@@ -1411,8 +1411,13 @@ func (sw *ShiftWrap) advanceShiftChange(s *Service, now time.Time) bool {
 				return false
 			}
 			sw.CalculateShiftChanges(s, t)
-			scn = NextShiftChange(now, s.shiftChanges)
-			// log.Printf("next shift change index for %s is %d", s.Name, scn)
+			// we use sc.At instead of now in the following, because the clock might have bypassed the times for some ShiftChanges,
+			// e.g. if one shift starts at the same time as another ends.
+			useTime := now
+			if !sc.IsZero() {
+				useTime = sc.At
+			}
+			scn = NextShiftChange(useTime, s.shiftChanges)
 			if scn >= 0 {
 				// we have a valid next shift change so add it and subsequent ones to the heap
 				sw.scQueue.Add(s.shiftChanges[scn:]...)
@@ -1441,7 +1446,7 @@ func (sw *ShiftWrap) doManageService(s *Service) {
 		return
 	}
 	now := sw.Clock.Now()
-	if !sw.advanceShiftChange(s, now) {
+	if !sw.advanceShiftChange(s, now, ShiftChange{}) {
 		log.Printf("refusing to manage service %s", s.Name)
 		return
 	}

commit 71b3dc579da294e5b4f18bc1995aac298efdcf16

avoid re-triggering the most recent shiftchange upon recalculation
- this bug was introduced in 51ba356c when we began supporting
back-to-back shifts (whose purpose is to allow Setup/Takedown code to
change how the service is running)

shiftwrap.go
@@ -769,14 +769,14 @@ func (sw *ShiftWrap) CalculateShiftChanges(s *Service, t time.Time) (rv time.Tim
 
 // NextShiftChange returns the index of the first ShiftChange in sc at or after
 // t, or -1 if there is no such ShiftChange (i.e. if all ShiftChanges in sc are before
-// t).
-func NextShiftChange(t time.Time, scs ShiftChanges) (rv int) {
+// t).  Also, the shiftchange must not have the same IsStart flag as the given shift change
+func NextShiftChange(t time.Time, scs ShiftChanges, sc ShiftChange) (rv int) {
 	if len(scs) == 0 {
 		return -1
 	}
 	rv = sort.Search(len(scs),
 		func(i int) bool {
-			return !scs[i].At.Before(t)
+			return !scs[i].At.Before(t) && (sc.IsZero() || scs[i].IsStart != sc.IsStart)
 		},
 	)
 	if rv == len(scs) {
@@ -1449,7 +1449,7 @@ func (sw *ShiftWrap) advanceShiftChange(s *Service, now time.Time, sc ShiftChang
 			if !sc.IsZero() {
 				useTime = sc.At
 			}
-			scn = NextShiftChange(useTime, s.shiftChanges)
+			scn = NextShiftChange(useTime, s.shiftChanges, sc)
 			if scn >= 0 {
 				// we have a valid next shift change so add it and subsequent ones to the heap
 				sw.scQueue.Add(s.shiftChanges[scn:]...)